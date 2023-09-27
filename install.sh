68b9acdae6d066f67721874bc379e14e4f2465ff
creytdeptrai <130132925+creyt2012@users.noreply.github.com>
2023-09-27 10:50:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 bash <(curl -Ls https://gitlab.com/qtai2901/xrayr/-/raw/main/install.sh)
-yum install -y wget && wget -O install.sh http://www.aapanel.com/script/install_6.0_en.sh && bash install.sh aapanel
+1
+
